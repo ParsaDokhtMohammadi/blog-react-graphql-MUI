d4923aa21f6d871326d453a96b55d0bbd3a3eec6
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-19 16:27:47 +0330

    apollo client added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import './index.css'
 import App from './App.jsx'
 
+const client = new ApolloClient({
+  uri : "https://ap-south-1.cdn.hygraph.com/content/cmq6u6gua001v06uunyvc0meu/master",
+  cache : new InMemoryCache()
+})
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>,
 )
