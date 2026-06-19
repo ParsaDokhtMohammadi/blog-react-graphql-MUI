d4868d244576a4644f230c38cd31b558587b650e
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-19 17:44:02 +0330

    fonts added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import './index.css'
+import './styles/index.css'
+import './styles/fonts.css'
 import App from './App.jsx'
 
 const client = new ApolloClient({
