3a6fbb5fd2e243e9c5593a9c04c2f036854aa372
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-22 19:49:07 +0330

    authors posts added to author page

--- src/components/author/AuthorPage.jsx
+++ src/components/author/AuthorPage.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { useParams } from 'react-router'
 import { GET_AUTHOR_BY_SLUG } from '../../graphql/queries'
 import { Avatar, Container, Grid, Typography } from '@mui/material'
+import sanitizeHtml from 'sanitize-html'
+import CardEL from '../shared/CardEL'
 
 const AuthorPage = () => {
 
@@ -15,15 +17,28 @@ const AuthorPage = () => {
     })
     if (loading) return <div>loading...</div>
     if (error) return <div>{error.message}</div>
+    const {author} = data
     return (
         <Container maxWidth="lg" sx={{ mt: 4 , display: "flex", flexDirection: "column", alignItems: "center" , justifyContent: "center" }}>
-            <Grid container>
+            <Grid container sx={{ mt: 4 , display: "flex", flexDirection: "column", alignItems: "center" , justifyContent: "center" }}>
                 <Grid xs={12} sx={{ mt: 4 , display: "flex", flexDirection: "column", alignItems: "center" , justifyContent: "center" }}>
-                    <Avatar src={data.author.avatar.url} alt={data.author.name} sx={{ mb: 2 ,width:250, height: 250 }} />
-                    <Typography component="h3" variant='h5' fontWeight={700}>{data.author.name}</Typography>
-                    <Typography component="p" variant='h5' fontWeight={700} color='textSecondary'>{data.author.field} </Typography>
+                    <Avatar src={author.avatar.url} alt={author.name} sx={{ mb: 2 ,width:250, height: 250 }} />
+                    <Typography component="h3" variant='h5' fontWeight={700}>{author.name}</Typography>
+                    <Typography component="p" variant='h5' fontWeight={700} color='textSecondary'>{author.field} </Typography>
                 </Grid>
-
+                <Grid xs={12}>
+                    <div dangerouslySetInnerHTML={{ __html: sanitizeHtml(author.description.html)  }}/>
+                </Grid>
+                <Grid xs={12} sx={{mt:6}}>
+                    <Typography component="h3" variant='h5' sx={{fontWeight:700}}>مقالات {author.name}</Typography>
+                    <Grid container spacing={2} sx={{mt:2, display:"flex", alignItems:"center" , justifyContent:"center" , width:"100%"}}>
+                        {author.posts.map(post=>(
+                            <Grid xs={12} sm={6} md={4} key={post.id}  >
+                                <CardEL data={post}></CardEL>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
             </Grid>
         </Container>
     )
--- src/components/shared/CardEL.jsx
+++ src/components/shared/CardEL.jsx
@@ -5,21 +5,24 @@ import { Link } from 'react-router'
 const CardEL = ({ data }) => {
     return (
         <Card sx={{ boxShadow: "rgba(0,0,0,0.2) 0px 4px 12px ", borderRadius: 4 }}>
-            <CardHeader
-                avatar={<Avatar src={data.author.avatar.url} alt={data.title} sx={{ ml: 2 }} />}
-                title={
-                    <Typography variant="p" component="p" color="textSecondary">{
-                        data.author.name
-                    }</Typography>
-                } />
+            {data.author && (
+                <CardHeader
+                    avatar={<Avatar src={data.author.avatar.url} alt={data.title} sx={{ ml: 2 }} />}
+                    title={
+                        <Typography variant="p" component="p" color="textSecondary">{
+                            data.author.name
+                        }</Typography>
+                    } />
+            )}
+
             <CardMedia component="img" height="194" image={data.coverImage.url} alt={data.title} loading="lazy" />
             <CardContent>
                 <Typography variant="h6" component="h3" color="textPrimary" fontWeight="600" sx={{ mb: 2 }}>{data.title}</Typography>
             </CardContent>
             <Divider variant='middle' sx={{ mb: 1 }} />
             <CardActions >
-                <Link to={`/blogs/${data.slug}`} style={{width:"100%"}}>
-                    <Button variant="outlined" size="small" color="primary" sx={{ width: "100%", borderRadius: 3 , fontWeight: 600 }}>
+                <Link to={`/blogs/${data.slug}`} style={{ width: "100%" }}>
+                    <Button variant="outlined" size="small" color="primary" sx={{ width: "100%", borderRadius: 3, fontWeight: 600 }}>
                         مطالعه مقاله
                     </Button>
                 </Link>
