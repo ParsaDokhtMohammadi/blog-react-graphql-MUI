4d7dadfd6623e5364b00359d7c70782d7aca66ff
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-22 17:00:31 +0330

    authors component craeted

--- src/components/author/Authors.jsx
+++ src/components/author/Authors.jsx
@@ -1,16 +1,34 @@
 import { useQuery } from '@apollo/client'
 import { GET_AUTHORS } from '../../graphql/queries'
+import { Avatar, Divider, Grid, Typography } from '@mui/material'
+import { Fragment } from 'react'
 
 const Authors = () => {
 
   const { loading, data, error } = useQuery(GET_AUTHORS)
 
-  if  (loading) return <div>loading...</div>
+  if (loading) return <div>loading...</div>
   if (error) return <div>{error.message}</div>
+  const { authors } = data
   return (
-    <div>
-      authors
-    </div>
+    <Grid container spacing={2} sx={{ p: 2, boxShadow: "rgba(0,0,0,0.2) 0px 4px 12px ", borderRadius: 4 }}>
+      {authors.map((author, index) => (
+        <Fragment key={author.id}>
+          <Grid size={{ xs: 12 }}>
+            <a href={`/author/${author.slug}`} style={{ display: "flex", alignItems: "center" }}>
+              <Avatar src={author.avatar.url} alt={author.name} sx={{ ml: 2 }} />
+              <Typography variant="p" component="p" color="textSecondary">{author.name}</Typography>
+            </a>
+          </Grid>
+          {index !== authors.length - 1 && (
+            <Grid size={{ xs: 12 }} >
+              <Divider sx={{ mb: 1 }} />
+            </Grid>
+          )}
+        </Fragment>
+      ))
+      }
+    </Grid >
   )
 }
 
