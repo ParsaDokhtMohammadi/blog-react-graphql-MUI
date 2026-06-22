175777703e03ce2e77135bd66d72d18646d9d7b4
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-22 18:02:40 +0330

    authors page updated

--- src/components/author/AuthorPage.jsx
+++ src/components/author/AuthorPage.jsx
@@ -2,10 +2,11 @@ import { useQuery } from '@apollo/client'
 import React from 'react'
 import { useParams } from 'react-router'
 import { GET_AUTHOR_BY_SLUG } from '../../graphql/queries'
+import { Avatar, Container, Grid, Typography } from '@mui/material'
 
 const AuthorPage = () => {
 
-    const {authorSlug} = useParams()
+    const { authorSlug } = useParams()
 
     const { loading, data, error } = useQuery(GET_AUTHOR_BY_SLUG, {
         variables: {
@@ -14,12 +15,18 @@ const AuthorPage = () => {
     })
     if (loading) return <div>loading...</div>
     if (error) return <div>{error.message}</div>
-    console.log(data)
-  return (
-    <div>
-      nice
-    </div>
-  )
+    return (
+        <Container maxWidth="lg" sx={{ mt: 4 , display: "flex", flexDirection: "column", alignItems: "center" , justifyContent: "center" }}>
+            <Grid container>
+                <Grid xs={12} sx={{ mt: 4 , display: "flex", flexDirection: "column", alignItems: "center" , justifyContent: "center" }}>
+                    <Avatar src={data.author.avatar.url} alt={data.author.name} sx={{ mb: 2 ,width:250, height: 250 }} />
+                    <Typography component="h3" variant='h5' fontWeight={700}>{data.author.name}</Typography>
+                    <Typography component="p" variant='h5' fontWeight={700} color='textSecondary'>{data.author.field} </Typography>
+                </Grid>
+
+            </Grid>
+        </Container>
+    )
 }
 
 export default AuthorPage
