a1b0f90c90e7ac4a3538ffd7897c46d680e5caf1
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-23 10:59:37 +0330

    blogPage added

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -1,11 +1,14 @@
 import { useQuery } from '@apollo/client'
 import React from 'react'
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 import { GET_POST_BY_SLUG } from '../../graphql/queries'
 import Loader from '../shared/Loader'
+import { Avatar, Box, Container, Grid, Typography } from '@mui/material'
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded'
+import sanitizeHtml from "sanitize-html"
 
 const BlogPage = () => {
-
+const navigate = useNavigate()
 const {blogSlug} = useParams()
 
 const { loading, data, error } = useQuery(GET_POST_BY_SLUG, {
@@ -17,12 +20,29 @@ const { loading, data, error } = useQuery(GET_POST_BY_SLUG, {
   if(loading) return <Loader/>
   if (error) return <div>{error.message}</div>
   const {post} = data
-  console.log(post)
 
   return (
-    <div>
-      
-    </div>
+    <Container >
+      <Grid container >
+        <Grid xs={12} sx={{mt:9 , display:"flex", justifyContent:"space-between" , alignItems:"center" ,width:"100%"}}>
+            <Typography component="h2" variant='h4' sx={{fontWeight:700}}>{post.title}</Typography>
+            <ArrowBackRoundedIcon OnClick={()=>navigate(-1)} sx={{cursor:"pointer"}}/>
+        </Grid>
+        <Grid xs={12} sx={{mt:6}}>
+          <img src={post.coverImage.url} alt={post.title} style={{width:"100%" , borderRadius:"15px"}}/>
+        </Grid>
+        <Grid xs={12} sx={{mt:6}} sx={{display:"flex" , alignItems:"center" ,width:"100%"}}>
+          <Avatar src={post.author.avatar.url} alt={post.author.name} sx={{ ml: 2 ,width:80, height: 80 }} />
+          <Box component="div">
+          <Typography variant="h5" component="p" sx={{ml:2}}>{post.author.name}</Typography>
+          <Typography variant="h5" component="p" color="textSecondary" sx={{ml:2}}>{post.author.field}</Typography>
+          </Box>
+        </Grid>
+        <Grid xs={12} sx={{mt:6}}>
+          <div dangerouslySetInnerHTML={{ __html: sanitizeHtml(post.content.html)  }}/>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
